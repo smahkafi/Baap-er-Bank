d10e2eb3ba0d5b7fdb10871f0419f8c537a17b1c
smahkafi <sakafi.m@gmail.com>
2021-08-20 21:05:01 +0600

    backup commoit

---
+++ js/banking.js
@@ -0,0 +1,55 @@
+
+// habdle deposit button event 
+document.getElementById('deposit-button').addEventListener('click', function(){
+    // get the amount deposited 
+    const depositInput = document.getElementById('deposit-input');
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
+
+    // update deposit total 
+    const depositTotal = document.getElementById('deposit-total');
+    const previousDepositAmountText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositAmountText);
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+
+    depositTotal.innerText = newDepositTotal;
+
+    // update account balance 
+
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+
+    balanceTotal.innerText = newBalanceTotal;
+    // clear the deposit input field 
+    depositInput.value = '';    
+
+});
+
+
+// handle withdraw event handler 
+
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText)
+    console.log(newWithdrawAmount);
+
+
+    // set withdraw total 
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText =  withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    withdrawInput.value = '';
+
+    // update balance 
+    const balanceTotal = document.getElementById('balance-total')
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+})
